c7a44214c05355c63a98c843707f5d6bdd7de748
Vedika Utekar <vedika@gmail.com>
2025-09-22 22:20:34 +0530

    quick fix

--- src/test/java/com/project/staragile/banking/BankingApplicationTests.java
+++ src/test/java/com/project/staragile/banking/BankingApplicationTests.java
@@ -1,7 +1,7 @@
 package com.project.staragile.banking;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootTest
 class BankingApplicationTests {
