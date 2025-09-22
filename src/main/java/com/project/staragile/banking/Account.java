04154db9ca0c2087a3c040559b08177ed39b2ca1
Vedika Utekar <vedika@gmail.com>
2025-09-22 22:01:27 +0530

    javax fix

--- src/main/java/com/project/staragile/banking/Account.java
+++ src/main/java/com/project/staragile/banking/Account.java
@@ -1,40 +1,45 @@
 package com.project.staragile.banking;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDate;
 
 @Entity
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int accountNumber;
+    private String accountNo;      // matches path variable
 
     private String accountName;
-    private String accountType;
-    private double balance;
+    private String policyType;     // renamed from accountType
+    private double sumInsured;     // renamed from balance
+    private LocalDate startDate;   // policy start date
 
     public Account() {}
 
-    public Account(int accountNumber, String accountName, String accountType, double balance) {
-        this.accountNumber = accountNumber;
+    public Account(String accountNo, String accountName, String policyType, double sumInsured, LocalDate startDate) {
+        this.accountNo = accountNo;
         this.accountName = accountName;
-        this.accountType = accountType;
-        this.balance = balance;
+        this.policyType = policyType;
+        this.sumInsured = sumInsured;
+        this.startDate = startDate;
     }
 
     // Getters & Setters
-    public int getAccountNumber() { return accountNumber; }
-    public void setAccountNumber(int accountNumber) { this.accountNumber = accountNumber; }
+    public String getAccountNo() { return accountNo; }
+    public void setAccountNo(String accountNo) { this.accountNo = accountNo; }
 
     public String getAccountName() { return accountName; }
     public void setAccountName(String accountName) { this.accountName = accountName; }
 
-    public String getAccountType() { return accountType; }
-    public void setAccountType(String accountType) { this.accountType = accountType; }
+    public String getPolicyType() { return policyType; }
+    public void setPolicyType(String policyType) { this.policyType = policyType; }
 
-    public double getBalance() { return balance; }
-    public void setBalance(double balance) { this.balance = balance; }
+    public double getSumInsured() { return sumInsured; }
+    public void setSumInsured(double sumInsured) { this.sumInsured = sumInsured; }
+
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
 }
