85f4a512854fdf75bf99f9db07f0069a38d39273
Vedika Utekar <vedika@gmail.com>
2025-09-22 22:03:23 +0530

    testaccount fix

--- src/test/java/com/project/staragile/banking/TestAccountService.java
+++ src/test/java/com/project/staragile/banking/TestAccountService.java
@@ -19,4 +19,30 @@ public class TestAccountService {
     @Test
     public void testCreateAccount() {
         Account account = new Account();
-        account.
+        account.setAccountNo("ACC2001");
+        account.setAccountName("Vedika");
+        account.setPolicyType("TERM");
+        account.setSumInsured(500000.0);
+        account.setStartDate(LocalDate.now());
+
+        Account saved = accountService.registerAccount(account);
+        assertNotNull(saved.getAccountNo(), "Account No should not be null");
+        assertEquals(saved.getAccountName(), "Vedika");
+        assertEquals(saved.getPolicyType(), "TERM");
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        // Preload account for test
+        Account account = accountService.getAccountDetails("ACC2001");
+        account.setSumInsured(700000.0);
+        Account updated = accountService.updateAccount(account.getAccountNo(), account);
+        assertEquals(updated.getSumInsured(), 700000.0);
+    }
+
+    @Test
+    public void testDeleteAccount() {
+        accountService.deleteAccount("ACC2001");
+        expectThrows(RuntimeException.class, () -> accountService.getAccountDetails("ACC2001"));
+    }
+}
